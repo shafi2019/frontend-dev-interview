7d9c827d5f51c14fea34ab79272e03dedbab22cf
Nate Folsom <nate@rhombussystems.com>
2023-03-08 12:05:28 -0500

    add register device mock api function

---
+++ src/state/registerDevice.ts
@@ -0,0 +1,28 @@
+export default function registerDevice(device: CreateDevice) {
+  return new Promise<{ deviceId: string }>((resolve, reject) => {
+    const apiCallFailed = getMockApiCallFailed();
+    const isValidDevice = validateDevice(device);
+
+    setTimeout(() => {
+      if (apiCallFailed || !isValidDevice) reject(new Error("An unexpected network error occurred"));
+      else resolve({ deviceId: generateDeviceId() });
+    }, 500)
+  })
+}
+
+const FAILURE_RATE = 0.5;
+function getMockApiCallFailed() {
+  return Math.random() <= FAILURE_RATE;
+}
+
+function generateDeviceId() {
+  return Array
+    .from({ length: 16 }, () => "abcdefghijklmnopqrstuvwxyz"[Math.round(Math.random() * 26)])
+    .join("");
+}
+
+type CreateDevice = { name: string; serialNumber: string; deviceType: string; };
+
+function validateDevice(device: CreateDevice) {
+  return !!device.name && !!device.serialNumber && !!device.deviceType;
+}
